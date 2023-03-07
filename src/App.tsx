4c635eb27504f7b9683a11fd1ca8aaed143cdbd2
Christopher David <chris@arcade.city>
2023-03-07 13:29:46 -0600

    Remove default branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
@@ -14,21 +13,9 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      <h1>NDXSTR</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <p>Feed me Nostr events.</p>
 
       <div className="row">
         <form
@@ -40,9 +27,9 @@ function App() {
           <input
             id="greet-input"
             onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
+            placeholder="Enter a relay URL..."
           />
-          <button type="submit">Greet</button>
+          <button type="submit">Index</button>
         </form>
       </div>
       <p>{greetMsg}</p>
